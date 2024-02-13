cfc4b833c06c122a265b5c209cb87502cadef7bf
Romyo282 <midoom282@gmail.com>
2024-02-13 08:35:01 +0200

    last update

---
+++ src/TextSummarizer/logging/__init__.py
@@ -0,0 +1,24 @@
+# Custom Log Using Builtin logging Model.
+import os
+import logging
+import sys
+
+# this will give you a message of logging informations like where you run the file
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log") # Make logging directory.
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("textSummarizerLogger")
